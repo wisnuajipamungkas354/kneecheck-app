444c2ea42b45f0504ad428191b322860ceeaf3e5
Wisnu Aji Pamungkas <wisnuajipamungkas354@gmail.com>
2024-11-11 13:39:16 +0700

    Adding app.yaml

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const Hapi = require('@hapi/hapi');
 const init = async () => {
 
     const server = Hapi.server({
-        port: 3000,
-        host: 'localhost'
+        port: 80,
+        host: 'https://knee-check-app.uw.r.appspot.com'
     });
 
     server.route({
