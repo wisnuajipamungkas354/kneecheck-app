278b4693f050d19a36ab4bc4080d74c887b01f9c
nopalss <holanakdo@gmail.com>
2024-11-18 12:58:28 +0700

    create middleware cek user and fix login

--- routes/routes.js
+++ routes/routes.js
@@ -11,6 +11,14 @@ import {
 } from "../controllers/pasienController.js";
 import { login } from "../controllers/loginController.js";
 import CekToken from "../middleware/cekToken.js";
+import {
+  getProfileDokter,
+  getAllDokter,
+  updateProfileDokter,
+  updateUserDokter,
+  getAllHistory,
+} from "../controllers/dokterController.js";
+import { cekTypeDokter, cekTypePasien } from "../middleware/cekUserType.js";
 const router = express.Router();
 
 router.get("/", CekToken, (req, res) => {
@@ -27,13 +35,31 @@ router.get("/about", (req, res) => {
 });
 
 // Pasien Routes
-router.get("/pasien/profile/:id", CekToken, getProfilePasien);
+router.get("/pasien/profile", CekToken, cekTypePasien, getProfilePasien);
+router.post("/pasien/profile/update-profile", CekToken, updateProfilePasien);
 router.post(
-  "/pasien/profile/:id/update-profile",
+  "/pasien/profile/update-user",
   CekToken,
-  updateProfilePasien
+  cekTypePasien,
+  updateUserPasien
 );
-router.post("/pasien/profile/:id/update-user", CekToken, updateUserPasien);
-router.get("/pasien/history/:id", CekToken, getHistoryPasien);
+router.get("/pasien/history/:id", CekToken, cekTypePasien, getHistoryPasien);
+
+// Dokter Routes
+router.get("/dokter", CekToken, cekTypeDokter, cekTypeDokter, getAllDokter);
+router.get("/dokter/history", CekToken, cekTypeDokter, getAllHistory);
+router.get("/dokter/profile", CekToken, cekTypeDokter, getProfileDokter);
+router.put(
+  "/dokter/profile/update-profile",
+  CekToken,
+  cekTypeDokter,
+  updateProfileDokter
+);
+router.put(
+  "/dokter/profile/update-user",
+  CekToken,
+  cekTypeDokter,
+  updateUserDokter
+);
 
 export default router;
